53559ef89a94139fe95ef33fe2129dc17952f1e7
klokssser <74603800+klokssser@users.noreply.github.com>
2025-03-31 15:50:12 +0500

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -28,7 +28,7 @@ vector<int> getCode(string& text, const int& n) {
 	string str5(text.begin() + 4 * n, text.begin() + 5 * n);
 	int fN = factorial(n);
 	
-	int maxCount{2};
+	int maxCount{1};
 	for (int i = 0; i < fN; ++i) {
 		string copyStr1(str1), copyStr2(str2), copyStr3(str3), copyStr4(str4), copyStr5(str5);
 
@@ -54,10 +54,9 @@ vector<int> getCode(string& text, const int& n) {
 		
 		std::ifstream in("10000-russian-words.txt");
 		int count{};
-		for (int j = 0; j < 10000; ++j) {
-			in >> russianWord;
+		while (in >> russianWord) {
 			for (int ii = 0; ii < vstrings.size(); ++ii)
-				if (vstrings[ii] == russianWord) {
+				if (vstrings[ii] == russianWord || vstrings[ii] == russianWord + ",") {
 					++count;
 					if (count > maxCount) {
 						maxCount = count;
@@ -71,41 +70,50 @@ vector<int> getCode(string& text, const int& n) {
 
 	}
 
-	return codes[codes.size()-1];
+	return codes.size() != 0 ? codes[codes.size()-1] : vector<int>();
 }
 
 void task2() {
 	ifstream in("task2.txt");
 	if (!in) {
-		cerr << "Ошибка открытия файла 2.txt" << std::endl;
+		cerr << "РћС€РёР±РєР° РѕС‚РєСЂС‹С‚РёСЏ С„Р°Р№Р»Р° 2.txt" << std::endl;
 		exit(-1);
 	}
 
 	string text{ std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>() };
 	remove_if(text.begin(), text.end(), [](char ch) {return ch == '\n'; }), text.end();
 
-	const int n = 6; // размер кодового блока
+	std::cout << "Р”РµРєРѕРґРёСЂРѕРІР°РЅРёРµ Р±Р»РѕС‡РЅРѕРіРѕ С€РёС„СЂР°, РІРІРµРґРёС‚Рµ СЂР°Р·РјРµСЂ РєР»СЋС‡Р°: ";
+	int n; // СЂР°Р·РјРµСЂ РєРѕРґРѕРІРѕРіРѕ Р±Р»РѕРєР°
+	std::cin >> n;
+	std::cout << "РџСЂРѕС†РµСЃСЃ РґРµРєРѕРґРёСЂРѕРІР°РЅРёСЏ...\n";
+
 
 	while (text.size() % n != 0)
 		text.push_back(' ');
 	
 	vector<int> code = getCode(text, n);
 
-	string res{};
-	for (int i = 0; i < text.size()-2; i += n) {
+	if (code.empty())
+		std::cout << "РўРµРєСЃС‚ РЅРµ РїРѕР»СѓС‡РёР»РѕСЃСЊ СЂР°СЃСЃРєРѕРґРёСЂРѕРІР°С‚СЊ.\n"
+					"РџСЂРѕРІРµСЂСЊС‚Рµ Р·Р°РєРѕРґРёСЂРѕРІР°РЅРЅС‹Р№ С‚РµРєСЃС‚ РёР»Рё РїРѕРїСЂРѕР±СѓР№С‚Рµ РґРѕР±Р°РІРёС‚СЊ СЂСѓСЃСЃРєРёС… СЃР»РѕРІ РІ 10000-russian-words.txt\n";
+	else {
+		string res{};
+		for (int i = 0; i < text.size() - 2; i += n) {
 
-		string subStr(text.begin() + i, text.begin() + i + n);
-		for (int j = 0; j < n; ++j)
-			res += subStr[code[j]];
-	
+			string subStr(text.begin() + i, text.begin() + i + n);
+			for (int j = 0; j < n; ++j)
+				res += subStr[code[j]];
+
+		}
+
+		cout << "РџРѕР»СѓС‡РµРЅ СЂР°СЃРєРѕРґРёСЂРѕРІР°РЅРЅС‹Р№ РєРѕРґ: ";
+		for (int i = 0; i < n; ++i)
+			cout << code[i] << ' ';
+
+
+		std::cout << "\n\nРџРѕР»СѓС‡РµРЅС‹Р№ С‚РµРєСЃС‚:\n" << res << "\n\nР•СЃР»Рё РїРѕР»СѓС‡РёР»СЃСЏ РЅРµРѕСЃРјС‹СЃР»РµРЅРЅС‹Р№ С‚РµРєСЃС‚ РґРѕР±Р°РІСЊС‚Рµ СЂСѓСЃСЃРєРёС… СЃР»РѕРІ РІ 10000-russian-words.txt"
+			"\nРёР»Рё РїСЂРѕРІРµСЂСЊС‚Рµ Р·Р°РєРѕРґРёСЂРѕРІР°РЅРЅС‹Р№ С‚РµРєСЃС‚." << std::endl;
 	}
-
-	cout << "Получен раскодированный код: ";
-	for (int i = 0; i < n; ++i)
-		cout << code[i] << ' ';
-
-
-	std::cout << "\n\nПолученый текст:\n" << res << "\n\nЕсли получился неосмысленный текст добавьте русских слов в 10000-russian-words.txt"
-		"\nили проверьте закодированный текст." << std::endl;
 
 }
